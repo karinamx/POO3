7df7743c8cfe8e4afc31f8bedd408b8a52738b7a
karinamx <134351593+karinamx@users.noreply.github.com>
2023-05-29 22:22:01 -0500

    Updated

---
+++ src/main/java/com/mycompany/main/Main.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.main;
+
+/**
+ *
+ * @author Gateway Blue
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        manipularPrograma obj = new manipularPrograma();
+        obj.menu();
+    }
+}
